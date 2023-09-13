565f8e53d49ad59b8fe6e861ba38d52687384667
Bread <86811307+Breadlicia@users.noreply.github.com>
2023-09-13 01:36:18 +0000

    message

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,14 +6,14 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 (module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://Breadlicia.github.io',
+  baseUrl: '/docusaurus',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'Breadlicia', // Usually your GitHub org/user name.
+  projectName: 'Breadlicia.github.io', // Usually your repo name.
+  trailingSlash: false
   presets: [
     [
       '@docusaurus/preset-classic',
