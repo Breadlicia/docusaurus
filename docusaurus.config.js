ddcb3f7c15f6af5cf15719d3019c45bca0489b66
Bread <86811307+Breadlicia@users.noreply.github.com>
2023-09-13 01:53:29 +0000

    changed the baseurl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://Breadlicia.github.io',
-  baseUrl: '/docusaurus/build/',
+  baseUrl: '/docusaurus/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
