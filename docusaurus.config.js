5b4f97143bb3b2ae834ba02d55ee20adcf0b1801
Bread <86811307+Breadlicia@users.noreply.github.com>
2023-09-14 16:05:55 +0000

    changed icon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,10 +41,10 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'run the bread',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/icon.png',
         },
         items: [
           {
