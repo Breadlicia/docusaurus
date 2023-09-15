8e3792a64655139d6b9b2012c0061d1f18544289
Bread <86811307+Breadlicia@users.noreply.github.com>
2023-09-15 22:11:20 +0000

    i think this might work

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/fundamentals/intro">
+            to="/docs/fundamentals/under construction">
             Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
