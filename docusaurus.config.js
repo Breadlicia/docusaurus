0d66a3753b775f9e2da47d0c6874c78ad5dfc8dd
Bread <86811307+Breadlicia@users.noreply.github.com>
2023-09-15 21:39:53 +0000

    testing error

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -69,7 +69,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro',
+                to: '/docs/congratulations',
               },
             ],
           },
