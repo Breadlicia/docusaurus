5661f19e6208c493df8ff52ca3b06da6dd69d831
Bread <86811307+Breadlicia@users.noreply.github.com>
2023-09-15 21:51:02 +0000

    error fix 2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,7 +49,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         items: [
           {
             type: 'doc',
-            docId: 'under construction',
+            docId: 'congratulations',
             position: 'left',
             label: 'Tutorial',
           },
