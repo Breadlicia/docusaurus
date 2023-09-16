ea034b18be1b2faba9026d88c35f19eceb2fab50
Bread <86811307+Breadlicia@users.noreply.github.com>
2023-09-16 00:44:18 +0000

    testing, might completely break code

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,9 +1,7 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
-// With JSDoc @type annotations, IDEs can provide config autocompletion
-/** @type {import('@docusaurus/types').DocusaurusConfig} */
-(module.exports = {
+module.exports = {
   title: 'Softdrop',
   tagline: 'Tetris Wiki',
   url: 'https://Breadlicia.github.io',
@@ -11,111 +9,105 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'Breadlicia', // Usually your GitHub org/user name.
-  projectName: 'Breadlicia.github.io', // Usually your repo name.
+  organizationName: 'Breadlicia',
+  projectName: 'Breadlicia.github.io',
   trailingSlash: true,
   presets: [
     [
       '@docusaurus/preset-classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
+      {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
           editUrl: 'https://github.com/breadlicia/docusaurus/edit/main/website/',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/breadlicia/docusaurus/edit/main/website/blog/',
+          editUrl: 'https://github.com/breadlicia/docusaurus/edit/main/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
-      }),
+      },
     ],
   ],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'Softdrop Wiki',
-        logo: {
-          alt: 'Softdrop Icon',
-          src: 'img/icon.png',
+  themeConfig: {
+    navbar: {
+      title: 'Softdrop Wiki',
+      logo: {
+        alt: 'Softdrop Icon',
+        src: 'img/icon.png',
+      },
+      items: [
+        {
+          type: 'doc',
+          docId: 'fundamentals/under construction',
+          position: 'left',
+          label: 'Tutorial',
         },
-        items: [
-          {
-            type: 'doc',
-            docId: 'fundamentals/under construction',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          { to: '/blog', label: 'Blog', position: 'left' },
-          {
-            href: 'https://github.com/breadlicia/docusaurus',
-            // Replace the text with an image
-            html: `
-              <img
-                src="/img/github-mark-white.png"
-                alt="GitHub"
-                style="height: 24px; width: auto;"
-              />
-            `,
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/fundamentals/under construction',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
-});
+        { to: '/blog', label: 'Blog', position: 'left' },
+        {
+          href: 'https://github.com/breadlicia/docusaurus',
+          // Replace the text with an image
+          html: `
+            <img
+              src="/img/github-mark-white.png"
+              alt="GitHub"
+              style="height: 24px; width: auto;"
+            />
+          `,
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/docs/fundamentals/under construction',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  },
+};
